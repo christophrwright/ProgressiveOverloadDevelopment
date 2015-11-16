c66518f772e652a519e0786f234dd2e828b5182c
evanisko <evanisko38@gmail.com>
2015-11-16 15:06:43 -0600

    History/AppDelegate Updates
    History now has dictionary implementation. App Delegate contains
    boilerplate code for supporting CoreData.
    
    ToDo:
      - Integrate History and Core Data so we can save our history
    permanently
      - Line graph stuff if we have time.

--- ProgressiveOverload_Draft/History.swift
+++ ProgressiveOverload_Draft/History.swift
@@ -1,8 +1,21 @@
-class History{
-    var exerciseHistory:[NSDate:Exercise]
-    var routineHistory:[NSDate:Routine]
+//
+//  History.swift
+//  ProgressiveOverload_Draft
+//
+//  Created by uics7 on 11/16/15.
+//  Copyright (c) 2015 Chris. All rights reserved.
+//
 
-    func saveExerciseSession(exercise){}
-    func saveWorkoutRoutine(routine){}
+import Foundation
+
+class History {
+    var myhistory = ["ExerciseName": [NSDate(), [0,0,0]]]
+    
+    
+    //needs to take actual exercise name and [sets/reps/weights] as arguments
+    func updateHistory(ExerciseName: String, userdata: [Int]) {
+        myhistory[ExerciseName] = [NSDate(), userdata]
+        //implement code to write current [sets,reps,weights] to csv here
+    }
 
 }
