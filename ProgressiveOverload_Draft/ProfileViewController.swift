9bf3d7eec982f0982edf877ebbfd4e73876304c6
christophrwright <christophrwright@gmail.com>
2015-11-17 12:34:39 -0600

    Update

---
+++ ProgressiveOverload_Draft/ProfileViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ProfileViewController.swift
+//  ProgressiveOverload_Draft
+//
+//  Created by Chris on 11/17/15.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+/*
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var age: UITextField!
+    @IBOutlet weak var inches: UITextField!
+    @IBOutlet weak var feet: UITextField!
+    @IBOutlet weak var weight: UITextField!
+   
+    func saveProfile(name: UITextField!, age:UITextField!, weight:UITextField!, feet:UITextField!, inches:UITextField!){
+        let name = name.text
+        let age = age.text
+        let weight = weight.text
+        let feet = feet.text
+        let inches = inches.text
+        let profile = [name, age, weight, feet, inches]
+        print(profile)
+    }
+    
+    */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
